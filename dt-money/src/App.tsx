241d521896812df775addb0044f7cfb913917755
Murilo Lopes <mloopes11@gmail.com>
2021-04-13 07:38:02 -0300

    estilizando texto com styled components

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`;
+
 export function App() {
   return (
     <div className="App">
-      <h1 className="title">Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
