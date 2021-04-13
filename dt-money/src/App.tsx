66b7a24a7ca4c8d7f91e41bc04586fcae8ee375a
Murilo Lopes <mloopes11@gmail.com>
2021-04-13 08:10:25 -0300

    cores da aplicaçao

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
+      <GlobalStyle />
     </div>
   );
 }
