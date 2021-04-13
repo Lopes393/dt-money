5cfd7a6fd119087860178a6eb52af73b3aee2376
Murilo Lopes <mloopes11@gmail.com>
2021-04-13 08:19:30 -0300

    font poppins

--- dt-money/src/styles/global.ts
+++ dt-money/src/styles/global.ts
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle`
 
         --text-tile: #363F5F;
         --text-body: #969CB3;
-        
+
         --shape: #FFFFFF;
     }
     *{
@@ -30,6 +30,13 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+    body, input, textarea, button {
+        font-family:'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1, h2, h3, h4, h5, h6, strong {
+       font-weight: 600; 
     }
     button {
         cursor: pointer;
