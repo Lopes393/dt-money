8438e4ed183aed2af678105a797e3674955eaf80
Murilo Lopes <mloopes11@gmail.com>
2021-04-13 23:39:17 -0300

    cabeçalho da aplicaçao finalizado

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <h1>HELLO WORLD</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
