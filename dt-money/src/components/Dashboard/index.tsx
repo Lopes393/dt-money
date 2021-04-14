50a66f3033d8f0c811f6c42fb6fe1450df4ca6d0
Murilo Lopes <mloopes11@gmail.com>
2021-04-14 08:19:05 -0300

    Dashboard, Sumary, Header, components

---
+++ dt-money/src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import { Sumary } from "../Sumary";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Sumary />
+    </Container>
+  );
+}
