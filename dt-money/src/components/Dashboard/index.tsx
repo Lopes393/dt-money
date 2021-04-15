a1564d14737ae3e9379c6922b414e0abde49e1c4
Murilo Lopes <mloopes11@gmail.com>
2021-04-14 23:57:01 -0300

    pagina estatica dt-money finalizada

--- dt-money/src/components/Dashboard/index.tsx
+++ dt-money/src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import { Container } from "./styles";
 import { Sumary } from "../Sumary";
+import React from "react";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <Container>
       <Sumary />
+      <TransactionsTable />
     </Container>
   );
 }
