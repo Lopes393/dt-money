8ed456e9dac256ce36474a165884719e2e689e97
Murilo Lopes <mloopes11@gmail.com>
2021-04-16 00:33:23 -0300

    Problemas com miragejs

--- dt-money/src/index.tsx
+++ dt-money/src/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import { App } from "./App";
 
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/trransactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createAd: new Date(),
+        },
+      ];
+    });
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
     <App />
