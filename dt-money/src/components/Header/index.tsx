e8db239d5ad860428eadaa05233a32ae80d3460f
Murilo Lopes <mloopes11@gmail.com>
2021-04-22 07:40:06 -0300

    Iniciando Modal

--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
 export function Header() {
+  const [isModalOpen] = useState();
   return (
     <Container>
       <Content>
