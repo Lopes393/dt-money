cc2558843674fe8f147684ef63e1332fbafe165a
Murilo Lopes <mloopes11@gmail.com>
2021-04-22 08:09:08 -0300

    Modal.setAppElement

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -5,6 +5,8 @@ import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 
+Modal.setAppElement("#root");
+
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
     false
