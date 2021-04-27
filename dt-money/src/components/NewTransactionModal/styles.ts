62ea6234e3c3fe54d529002c6a42a10bb31d8629
Murilo Lopes <mloopes11@gmail.com>
2021-04-26 22:45:04 -0300

    styled buttons modal

--- dt-money/src/components/NewTransactionModal/styles.ts
+++ dt-money/src/components/NewTransactionModal/styles.ts
@@ -48,4 +48,9 @@ export const Container = styled.form`
   }
 `;
 
-export const TransactionTypeContainer = styled.div``;
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
